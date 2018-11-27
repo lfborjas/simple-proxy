37030787cf8c41ff96eca7957ee83128d449887b
Luis Borjas <luis@birchbox.com>
2018-11-26 19:04:32 -0500

    turns out we need to define fns before using it, who knew

--- server.lisp
+++ server.lisp
@@ -10,6 +10,18 @@
 ;; handlers defined by define-easy-handler won't be picked up.
 (defparameter *server* (make-instance 'hunchentoot:easy-acceptor :port 8666))
 
+;; This could be made fancier by also sending over the headers, etc, but we have some
+;; knowledge of what exactly we're dealing with here (a crappy server that don't care 'bout nothin')
+(defun handle-request (url body)
+  ;; drakma:http-request actually returns multiple values, we could look into
+  ;; leveraging those to return the right status code, headers, etc.
+  ;; more options at: https://edicl.github.io/drakma/#http-request
+  (let ((response (drakma:http-request url
+                                       :method :post
+                                       :content body
+                                       :content-type "text/xml")))
+    response))
+
 ;; Main handler
 (hunchentoot:define-easy-handler (proxy-request :uri "/proxy-request") (url timeout)
   ;; if they want a timeout, assume they meant minutes and do just that:
@@ -23,17 +35,6 @@
     (setf (hunchentoot:content-type*) "text/xml")
     response))
 
-;; This could be made fancier by also sending over the headers, etc, but we have some
-;; knowledge of what exactly we're dealing with here (a crappy server that don't care 'bout nothin')
-(defun handle-request (url body)
-  ;; drakma:http-request actually returns multiple values, we could look into
-  ;; leveraging those to return the right status code, headers, etc.
-  ;; more options at: https://edicl.github.io/drakma/#http-request
-  (let ((response (drakma:http-request url
-                                       :method :post
-                                       :content body
-                                       :content-type "text/xml")))
-    response))
 
 (hunchentoot:start *server*)
-;;(hunchentoot:stop *server*)
+(princ "Call (hunchentoot:stop *server*) anytime to stop the server I just started!")
